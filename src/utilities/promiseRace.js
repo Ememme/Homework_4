27adc602dbfa3a2e8e2f49e9bcbb969fe209c17d
Magda Mieczynska <magda.mieczynska@gmail.com>
2019-04-18 16:55:46 +0200

    Seperate catch to see if gh pages will work

--- src/utilities/promiseRace.js
+++ src/utilities/promiseRace.js
@@ -2,7 +2,7 @@ const promiseRace = async(promises) => {
     return new Promise((resolve, reject) => {
       for (let item of promises) {
         if (item != null && item instanceof Promise) {
-          item.then(resolve, reject);
+          item.then(resolve).catch(reject);
         }
         else {
           return resolve(item);
@@ -12,3 +12,5 @@ const promiseRace = async(promises) => {
 };
 
 export default promiseRace;
+
+// ememme.github.io/:1 Uncaught (in promise) Test 2 promise.race:// Za wolno!
